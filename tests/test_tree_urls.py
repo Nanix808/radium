755f75bb67deba4cd38e3afe9fcd5aba6577eb63
nanix808 <bratan_tirex@gmail.com>
2024-03-21 12:25:12 +0300

    added test

--- tests/test_tree_urls.py
+++ tests/test_tree_urls.py
@@ -6,93 +6,89 @@ import asyncio
 from pathlib import Path
 from main import main, tree_urls, download_file, calculate_sha256
 from requests.exceptions import Timeout
+from requests.exceptions import RequestException
+from unittest.mock import AsyncMock
 
 
-def test_valid_url_and_folder_name():
-
-    with patch("requests.get") as mock_get:
-        mock_response = [{"type": "file", "url": "file_url", "name": "file_name"}]
-        mock_get.return_value.json.return_value = mock_response
-        expected_result = [{"folder": "", "url": "file_url", "name": "file_name"}]
-        assert tree_urls("valid_url", "folder_name") == expected_result
-
-
-# def test_invalid_url():
-#     with patch("requests.get") as mock_get:
-#         mock_get.side_effect = Timeout
-#         with pytest.raises(Timeout):
-#             tree_urls("invalid_url")
+@patch("main.requests.get")
+def test_valid_url_and_folder_name(mock_get):
+    mock_response = [{"type": "file", "url": "file_url", "name": "file_name"}]
+    mock_get.return_value.json.return_value = mock_response
+    expected_result = [
+        {"folder": "folder_name", "url": "file_url", "name": "file_name"}
+    ]
+    assert tree_urls("valid_url", "folder_name") == expected_result
 
 
-# def test_empty_folder_name():
-#     with patch("requests.get") as mock_get:
-#         mock_response = [{"type": "file", "url": "file_url", "name": "file_name"}]
-#         mock_get.return_value.json.return_value = mock_response
-#         expected_result = [{"folder": "", "url": "file_url", "name": "file_name"}]
-#         assert tree_urls("valid_url") == expected_result
+@patch("main.requests.get")
+def test_invalid_url(mock_get):
+    mock_get.side_effect = Timeout
+    with pytest.raises(Timeout):
+        tree_urls("invalid_url")
 
 
-# @pytest.mark.asyncio
-# async def test_main_repository_folder_creation():
-#     with patch("aiohttp.ClientSession") as mock_session:
-#         mock_session.return_value = MagicMock()
-#         with patch("pathlib.Path.mkdir") as mock_mkdir:
-#             await main()
-#             mock_mkdir.assert_called_once_with(exist_ok=True)
+@patch("main.requests.get")
+def test_empty_folder_name(mock_get):
+    mock_response = [{"type": "file", "url": "file_url", "name": "file_name"}]
+    mock_get.return_value.json.return_value = mock_response
+    expected_result = [{"folder": "", "url": "file_url", "name": "file_name"}]
+    assert tree_urls("valid_url") == expected_result
 
 
-# @pytest.mark.asyncio
-# async def test_main_tree_urls_retrieval():
-#     with patch("aiohttp.ClientSession") as mock_session:
-#         mock_session.return_value = MagicMock()
-#         with patch("pathlib.Path.mkdir"):
-#             with patch("main_module.tree_urls") as mock_tree_urls:
-#                 await main()
-#                 mock_tree_urls.assert_called_once_with(
-#                     "https://gitea.radium.group/api/v1/repos/radium/project-configuration/contents"
-#                 )
+def test_expected_params():
+    with pytest.raises(TypeError):
+        tree_urls()
 
 
-# @pytest.mark.asyncio
-# async def test_main_file_downloading():
-#     url_list = [{"url": "url1"}, {"url": "url2"}, {"url": "url3"}]
-#     with patch("aiohttp.ClientSession") as mock_session:
-#         mock_session.return_value = MagicMock()
-#         with patch("pathlib.Path.mkdir"):
-#             with patch("main_module.tree_urls") as mock_tree_urls:
-#                 mock_tree_urls.return_value = url_list
-#                 with patch("main_module.download_file") as mock_download_file:
-#                     await main()
-#                     mock_download_file.assert_has_calls(
-#                         [
-#                             unittest.mock.call(mock_session, "url1", Path("repo")),
-#                             unittest.mock.call(mock_session, "url2", Path("repo")),
-#                             unittest.mock.call(mock_session, "url3", Path("repo")),
-#                         ]
-#                     )
+@pytest.mark.asyncio
+async def test_download_file_success():
+    mock_response = AsyncMock()
+    mock_response.status = 200
+
+    mock_session = AsyncMock()
+    mock_session.get.return_value.__aenter__.return_value = mock_response
+
+    url_info = {"url": "https://example.com", "folder": "folder", "name": "file.txt"}
+    url_list = [url_info]
+    repo_dir = "repo"
+
+    await download_file(mock_session, url_list, repo_dir)
+
+    file_path = os.path.join(repo_dir, url_info["folder"], url_info["name"])
+    assert os.path.exists(file_path)
 
 
 # @pytest.mark.asyncio
-# async def test_main_sha256_calculation():
-#     url_list = [
-#         {"folder": "folder1", "name": "file1"},
-#         {"folder": "folder2", "name": "file2"},
-#     ]
-#     with patch("aiohttp.ClientSession") as mock_session:
-#         mock_session.return_value = MagicMock()
-#         with patch("pathlib.Path.mkdir"):
-#             with patch("main_module.tree_urls") as mock_tree_urls:
-#                 mock_tree_urls.return_value = url_list
-#                 with patch("main_module.download_file"):
-#                     with patch("main_module.calculate_sha256") as mock_calculate_sha256:
-#                         await main()
-#                         mock_calculate_sha256.assert_has_calls(
-#                             [
-#                                 unittest.mock.call(
-#                                     os.path.join(Path("repo"), "folder1", "file1")
-#                                 ),
-#                                 unittest.mock.call(
-#                                     os.path.join(Path("repo"), "folder2", "file2")
-#                                 ),
-#                             ]
-#                         )
+# async def test_download_file_404():
+#     mock_response = AsyncMock()
+#     mock_response.status = 404
+
+#     mock_session = AsyncMock()
+#     mock_session.get.return_value.__aenter__.return_value = mock_response
+
+#     url_info = {'url': 'https://example.com', 'folder': 'folder', 'name': 'file.txt'}
+#     url_list = [url_info]
+#     repo_dir = '/path/to/repo'
+
+#     await download_file(mock_session, url_list, repo_dir)
+
+#     file_path = os.path.join(repo_dir, url_info['folder'], url_info['name'])
+#     assert not os.path.exists(file_path)
+
+# @pytest.mark.asyncio
+# async def test_download_file_save_to_correct_directory():
+#     mock_response = AsyncMock()
+#     mock_response.status = 200
+
+#     mock_session = AsyncMock()
+#     mock_session.get.return_value.__aenter__.return_value = mock_response
+
+#     url_info = {'url': 'https://example.com', 'folder': 'folder', 'name': 'file.txt'}
+#     url_list = [url_info]
+#     repo_dir = '/path/to/repo'
+
+#     await download_file(mock_session, url_list, repo_dir)
+
+#     file_path = os.path.join(repo_dir, url_info['folder'], url_info['name'])
+#     assert os.path.exists(file_path)
+#     assert os.path.dirname(file_path) == os.path.join(repo_dir, url_info['folder'])
